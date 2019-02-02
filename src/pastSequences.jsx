7dbb103b8b905021dc7ddd50a4a07080b1353404
travis <trav.horn@gmail.com>
2019-02-02 10:27:15 -0700

    Better display of PastFlows

--- src/pastSequences.jsx
+++ src/pastSequences.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import firebase from './firebase.js'; // <--- add this line
 
+const PastFlow = styled.div`
+  position: relative;
+  width: calc(25% - 4px);
+  margin-right: 4px;
+  margin-bottom: 4px;
+  padding: 20px;
+  border: 2px solid grey;
+  display: inline-block;
+  height: 160px;
+  vertical-align: middle;
+`;
 
 
 export default class PastSequences extends React.Component {
@@ -12,15 +23,14 @@ export default class PastSequences extends React.Component {
     return (
       <section className='display-item'>
         <div className="wrapper">
-          <ul>
             {this.props.pastFlows.map((item) => {
               return (
-                <li key={item.id}>
-                  <p>created: {item.flowTitle}</p>
-                </li>
+                  <PastFlow key={item.id} className='card'>
+                    <h4>{item.flowTitle}</h4>
+                    <small>{item.creationDate}</small>
+                  </PastFlow>
               )
             })}
-          </ul>
         </div>
       </section>
     );
